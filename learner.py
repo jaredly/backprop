d87087fe318544e8ca545cf7ea2018d456e97630
Jared Forsyth <jared@jaredforsyth.com>
2014-02-15 12:44:01 -0700

    baseline learner

--- learner.py
+++ learner.py
@@ -8,30 +8,45 @@ class Learner:
         self.best_state = None
         self.target = target
         self.meta = meta
+        self.init_state()
+
+    # OVERRIDE THESE
+    def init_state(self):
+        self.seen = {}
+        self.most = None
     
     def state(self):
-        raise Exception('Override')
+        '''get current state'''
+        return self.seen.copy()
 
     def use_best(self):
-        raise Exception('Override')
+        '''set state to the best so far'''
+        self.seen = self.best_state
 
-    def train(self, data):
-        '''test a line, return 1/0 for right/wrong'''
-        raise Exception('Override')
+    def train(self, data, target):
+        '''test a line, return True/False for right/wrong'''
+        if self.most == target:
+            return True
+        self.seen[target] = self.seen.get(target, 0) + 1
+        items = self.seen.items()
+        items.sort(lambda a,b: b[1] - a[1])
+        self.most = items[0][0]
 
-    def epoch(self, data):
+    def classify(self, line):
+        return self.most
+
+    # don't need to override
+    def epoch(self, data, targets):
+        '''run an epoch on the data. Calleds "train(line)"'''
         results = []
         for i in tdata.index:
-            results.append(self.train(tdata.loc[i]))
+            results.append(1 if self.train(tdata.loc[i], targets.loc[i]) else 0)
         accuracy = sum(results)/float(len(results))
         weights = self.state()
         if accuracy > self.best:
             self.best = accuracy
             self.best_state = weights
         return accuracy, weights
-
-    def classify(self, line):
-        raise Exception('Override')
 
 
 
---
+++ test_learner.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import pytest
+from pandas import DataFrame, Series
+from scipy.io.arff import loadarff
+
+from learner import Learner
+
+@pytest.fixture()
+def ex1():
+    data, meta = loadarff('test.arff')
+    data = DataFrame(data)
+    return data, meta
+
+@pytest.fixture()
+def simple(ex1):
+    return Learner(ex1[1])
+
+def test_learner(ex1, simple):
+    assert not simple.train(None, 'hat')
+    assert not simple.train(None, 'coat')
+    assert not simple.train(None, 'jacket')
+    simple.train(None, 'coat')
+    assert simple.classify(None) == 'coat'
+
+
+# vim: et sw=4 sts=4
